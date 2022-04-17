309d1831b333cb962c65add8db9fb4f3483c8898
Dan Finlay <dan@danfinlay.com>
2022-04-16 20:02:17 -0700

    Clean up undesired MetaMask GitHub files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
+import {
+  recoverTypedSignature,
+  signTypedData,
+  TypedDataUtils,
+  typedSignatureHash,
+  SignTypedDataVersion,
+} from './eth-sig-util';
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
-    case 'hello':
-      return wallet.request({
+    case 'signTypedData_v5':
+      const typedMessage = requestObject.params[0];
+
+      const consent = await wallet.request({
         method: 'snap_confirm',
         params: [
           {
-            prompt: `Hello, ${originString}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
+            prompt: `Signature Requested`,
+            description: `From the site at ${originString}`,
             textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+              'This is the raw proposed signature data, it isn\'t going to be very readable here just yet, but it will get better, and it\'s a lot better than nothing:\n\n' + JSON.stringify(typedMessage, null, 2),
           },
         ],
       });
+
+      if (!consent) {
+        return false;
+      }
+
+      const coinTypeNode = await wallet.request({
+        method: 'snap_getBip44Entropy_60'
+      });
+      const privateKey = coinTypeNode.key;
+
+      const signature = signTypedData({
+        privateKey,
+        data: typedMessage,
+        version: SignTypedDataVersion.V4,
+      });
+
+      return signature;
+
     default:
-      throw new Error('Method not found.');
+      throw new Error('Method not found: ' + requestObject.method);
   }
 });
